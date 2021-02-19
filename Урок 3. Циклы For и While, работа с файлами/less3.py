32ae3cfd33792061596ce1dd40e9b712e1b8b4d2
Alinka <alishasergeeva94@gmail.com>
2021-02-19 22:14:11 +0200

    edit file less3

--- Урок 3. Циклы For и While, работа с файлами/less3.py
+++ Урок 3. Циклы For и While, работа с файлами/less3.py
@@ -1,39 +1,43 @@
-# sum = 0
-# a = [1, 2, 3 ,4 ,5]
-# for i in a:
-#  	sum += i
-# print(sum)
+# 1.Каждый пишет сумму списка при помощи for и while
+
+sum = 0
+a = [1, 2, 3 ,4 ,5]
+for i in a:
+ 	sum += i
+print(sum)
 
 
-# sum = 0
-# index = 0
-# l = [1, 2, 3, 4, 5]
+sum = 0
+index = 0
+l = [1, 2, 3, 4, 5]
 
-# list_last_index = len(l)
-# while index < list_last_index:
-#     sum += l[index]
-#     index += 1
-# print(sum)
+list_last_index = len(l)
+while index < list_last_index:
+    sum += l[index]
+    index += 1
+print(sum)
 
+# 2.Написать программу, которая выводит сама себя
 
-# import sys
-# filename = sys.argv[1]
-# f = open(filename, 'r')
-# for line in f:
-# 	print(line)
-# f.close()
+import sys
+filename = sys.argv[1]
+f = open(filename, 'r')
+for line in f:
+	print(line)
+f.close()
 
+# 3.Написать программу, которая выводит саму себя задом наперед
 
-# import sys
-# filename = sys.argv[1]
-# f = open(filename, 'r')
-# for line in f:
-# 	print(line[::-1])
-# f.close()
+filename = sys.argv[1]
+f = open(filename, 'r')
+for line in f:
+	print(line[::-1])
+f.close()
 
-# import sys
-# filename = sys.argv[0]
-# f = open(filename, 'r')
-# for line in f:
-# 	print(line)
-# f.close()
+# или:
+
+filename = sys.argv[0]
+f = open(filename, 'r')
+for line in f:
+	print(line)
+f.close()
