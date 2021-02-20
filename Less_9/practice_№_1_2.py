afb3df9ca3bac01dac63c1f4d13f9d7e673475b3
Alinka <alishasergeeva94@gmail.com>
2021-02-20 12:39:47 +0200

    delite extra variable

--- Less_9/practice_№_1_2.py
+++ Less_9/practice_№_1_2.py
@@ -1,22 +1,24 @@
 
 # Задание 1:
-# import math
-# number = int(input('Please enter a number: '))
-# floors = 9
-# entrance = 4 
-# number_of_apartments_per_floor = 4
 
-# def my_func(number):
+import math
+number = int(input('Please enter a number: '))
+floors = 9
+number_of_apartments_per_floor = 4
 
-# 	appartments_per_entrsnce = number_of_apartments_per_floor * floors
-# 	result_entrace = math.ceil(number / appartments_per_entrsnce)
-# 	print('Entrance: ' + str(result_entrace))
-# 	result_floor = (result_entrace * appartments_per_entrsnce - number) // number_of_apartments_per_floor
-# 	print('Floor: ' + str(floors - result_floor))
 
-# my_func(number)
+def my_func(number):
+
+	appartments_per_entrsnce = number_of_apartments_per_floor * floors
+	result_entrace = math.ceil(number / appartments_per_entrsnce)
+	print('Entrance: ' + str(result_entrace))
+	result_floor = (result_entrace * appartments_per_entrsnce - number) // number_of_apartments_per_floor
+	print('Floor: ' + str(floors - result_floor))
+
+my_func(number)
 
 # Задание 2
+
 num = int(input("Please enter an odd positive number: "))
 if num < 0 or (num % 2) == 0:
     print('Error!!!')
